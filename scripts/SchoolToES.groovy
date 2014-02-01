9566dcf4076e343d0b0be4cb7c557a88f319f8eb
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-02-01 15:59:07 +0000

    updated

--- scripts/SchoolToES.groovy
+++ scripts/SchoolToES.groovy
@@ -1,7 +1,9 @@
 @Grapes([
     @Grab(group='net.sourceforge.nekohtml', module='nekohtml', version='1.9.14'),
     @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1'),
-    @Grab(group='xerces', module='xercesImpl', version='2.9.1') ])
+    @Grab(group='xerces', module='xercesImpl', version='2.9.1'),
+    @Grab(group='org.elasticsearch', module='elasticsearch-lang-groovy', version='1.4.0') ])
+
  
 import groovyx.net.http.*
 import static groovyx.net.http.ContentType.URLENC
@@ -12,16 +14,38 @@ import org.apache.http.entity.mime.*
 import org.apache.http.entity.mime.content.*
 import java.nio.charset.Charset
 import static groovy.json.JsonOutput.*
+import org.elasticsearch.groovy.client.GClient 
+import org.elasticsearch.groovy.node.GNode 
+import org.elasticsearch.groovy.node.GNodeBuilder 
+import static org.elasticsearch.groovy.node.GNodeBuilder.* 
  
 def the_base_url = "http://education.data.gov.uk"
 // /sparql/education/query?query=PREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+sch-ont%3A+%3Chttp%3A%2F%2Feducation.data.gov.uk%2Fdef%2Fschool%2F%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0A%0D%0ASELECT+%3Fname+%3Flat+%3Flon+%3FlaUri+%3FlaName+%3Furn+%3Fiip+%3FadmissionsPolicy+%3FstatLowAge+%3FstatHighAge+%7B%0D%0A%3Fs+a+sch-ont%3ASchool.%0D%0A%3Fs+rdfs%3Alabel+%3Fname+.%0D%0A%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23lat%3E+%3Flat.%0D%0A%3Fs+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23lat%3E+%3Flon.%0D%0A%3Fs+sch-ont%3AlocalAuthority+%3FlaUri+.%0D%0A%3FlaUri+rdfs%3Alabel+%3FlaName.%0D%0A%3Fs+sch-ont%3AuniqueReferenceNumber+%3Furn+.%0D%0A%3Fs+sch-ont%3AinvestorInPeople+%3Fiip.%0D%0A%3Fs+sch-ont%3AadmissionsPolicy+%3FadmissionsPolicy+.%0D%0A%3Fs+sch-ont%3AstatutoryLowAge+%3FstatLowAge+.%0D%0A%3Fs+sch-ont%3AstatutoryHighAge+%3FstatHighAge+.%0D%0A%7D+%0D%0A&output=json&stylesheet=xml-to-html.xsl&force-accept=text%2Fplain"
  
-processTopLevel(the_base_url);
+GNodeBuilder nodeBuilder = nodeBuilder(); 
+
+nodeBuilder.settings { 
+    node { 
+        client = true 
+    } 
+    cluster { 
+        name = "iihubble" 
+    } 
+} 
+
+println("Get node...");
+GNode node = nodeBuilder.node() 
+println("Process");
+processTopLevel(the_base_url, node);
+println("Stop node");
+node.stop().close() 
  
-def processTopLevel(base_url) {
+def processTopLevel(base_url, esnode) {
+
   println "Loading page"
   def http = new HTTPBuilder( base_url )
  
+  println("Get...");
   // perform a GET request, expecting JSON response data
   http.request( GET, JSON ) {
     uri.path = '/sparql/education/query'
@@ -46,7 +70,8 @@ SELECT ?name ?lat ?lon ?laUri ?laName ?urn ?iip ?admissionsPolicy ?statLowAge ?s
 } 
 """,
                   output:'json']
-  
+
+
     // headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
   
     // response handler for a success response code:
